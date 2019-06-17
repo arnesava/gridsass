02d2eac121aab249e0de9bc7dae783eeb83b2dee
savalina <alina@sava.io>
2019-06-18 00:26:26 +0200

    add sass-loader, style-loader, node-sass cleanup (deleted package-lock.json)

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,35 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 module.exports = {
-	optimization: {
-		minimizer: [
-			new OptimizeCSSAssetsPlugin({
-				cssProcessorPluginOptions: {
-					preset: ['default', {colormin: false}],
-				},
-			})
-		]
-	},
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: "gridkit.css",
-			chunkFilename: "[id].css"
-		}),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					"css-loader"
-				]
-			}
-		]
-	}
-}
+    optimization: {
+        minimizer: [
+            new OptimizeCSSAssetsPlugin({
+                cssProcessorPluginOptions: {
+                    preset: ["default", { colormin: false }]
+                }
+            })
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "gridkit.css",
+            chunkFilename: "[id].css"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            }
+        ]
+    }
+};
